d67a2db17cebab5d172975d4eb23f688eab9bb4a
Vowowow <394775325@qq.com>
2020-05-26 07:28:47 +0000

    test

---
+++ caogao.py
@@ -0,0 +1,10 @@
+from PIL import Image
+import numpy as np
+import matplotlib.pyplot as plt
+
+image = Image.open('../../data/line_detect/JPEGImages/14.bmp').convert('RGB')
+image,_ ,_  = image.split()
+# testtest!
+plt.imshow(image) # 显示图片
+# plt.axis('off') # 不显示坐标轴
+plt.show()
